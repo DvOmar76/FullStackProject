127c471fcd44d72076511083d8a70cd34f6571af
DvOmar76 <84674856+DvOmar76@users.noreply.github.com>
2021-12-30 19:03:29 +0300

    Initial Commit

---
+++ FullStackProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FullStackProject
+//
+//  Created by DvOmar on 26/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
